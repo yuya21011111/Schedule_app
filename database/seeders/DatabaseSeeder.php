b3bb6b7854a6dc0a9e2a13d8eba9a2710f0915cf
YuyaYamazi <kaimomo12345@gmail.com>
2023-03-26 15:58:14 +0900

    factory ダミーデータ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Event;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,5 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
         ]);
+
+        Event::factory(100)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivewireTestController;
 use App\Http\Controllers\AlpineTestController;
+use App\Http\Controllers\EventController;
+use Barryvdh\Debugbar\DataCollector\EventCollector;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,9 +33,8 @@ Route::middleware([
 
 Route::prefix('manager')
 ->middleware('can:manager-higher')->group(function(){
-    Route::get('index',function(){
-        dd('manager');
-    });
+    Route::resource('events',EventCollector::class);
+    
 });
 
 Route::middleware('can:user-higher')->group(function(){
